2ec386c602f68f4cea3c761469ac38b699257f17
Saideekshitha <104785671+Saideekshithach@users.noreply.github.com>
2024-11-06 19:13:50 +0530

    Add files via upload

---
+++ realtimeexamples on condition statements.py
@@ -0,0 +1,105 @@
+#voting
+'''while True:
+    age=int(input("enter age"))
+    if age>=18:
+        print("eligible for voting")
+    else:
+        print("not eligible for voting")'''
+
+#even or odd
+'''while True:
+    num=int(input("enter number:"))
+    if num%2==0:
+        print("even")
+    else:
+        print("odd")'''
+#guest code
+'''while True:
+    name=input("enter name:")
+    if name=="Deekshi":
+        print("Welcome",name)
+    else:
+        print("welcome guest")'''
+#leap year
+'''while True:
+    year=int(input("enter year:"))
+    if year%4==0:
+        print("leap year")
+    else:
+        print("not a leap year")'''
+#social media login
+'''while True:
+    username=input("enter the username")
+    password=int(input("enter the password"))
+    if username=="pooja" and password==1234:
+        print("login successful")
+    else:
+        print("invalid credentials")'''
+'''while True:
+    username=input("enter the username")
+    password=int(input("enter the password"))
+    username="pooja"
+    password=1234
+    if username==username and password==password:
+        print("login successful")
+    else:
+        print("invalid credentials")'''
+
+'''while True:
+    username=input("enter the username")
+    password=input("enter the password")
+    if username=="pooja" and password=='pooja@1234':
+        print("login successful")
+    else:
+        print("invalid credentials")'''
+#week days
+'''while True:
+    Day=input("enter a day")
+    if Day=='Monday':
+        print("working day")
+    elif Day=="Thursday":
+        print("middle of the week")
+    elif Day=="Saturday":
+        print("weekend")
+    elif Day=="Sunday":
+        print("funday")
+    else:
+        print("Boring days")'''
+
+'''while True:
+    Day=input("enter a day")
+    if Day=='monday' or Day=='Monday':
+        print("working day")
+    elif Day=="Thursday" or Day=='thursday':
+        print("middle of the week")
+    elif Day=="Saturday" or Day=='saturday':
+        print("weekend")
+    elif Day=="Sunday" or Day=='sunday':
+        print("funday")
+    else:
+        print("Boring days")'''
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
